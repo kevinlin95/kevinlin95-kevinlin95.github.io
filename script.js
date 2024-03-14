14f76b72be044522a3a3e25b16e53a71cf294b65
kevinlin <contact.kevinlin95@gmail.com>
2024-03-13 22:45:46 -0400

    3/13 updating directory

--- script.js
+++ script.js
@@ -2,45 +2,16 @@
 // Used to toggle the menu on small screens when clicking on the menu button
 function mobileMenuToggle() {
     const x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") === -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+    x.classList.toggle("w3-show");
 }
 
-const startButton = document.getElementById('start-button');
-startButton.addEventListener('click', () => {
-});
+document.getElementById("toggle").addEventListener("click", mobileMenuToggle);
 
-function handleFormSubmission(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    // Get form data
-    var formData = new FormData(this);
-
-    // You can perform further validation or processing here
-
-    // Send form data using fetch or XMLHttpRequest
-    fetch("your-server-endpoint", {
-        method: "POST",
-        body: formData
-    })
-        .then(response => {
-            if (response.ok) {
-                alert("Form submitted successfully!");
-                // Reset the form after successful submission
-                document.getElementById("contactForm").reset();
-            } else {
-                throw new Error("Error in form submission.");
-            }
-        })
-        .catch(error => {
-            console.error("Form submission error:", error);
-            alert("There was an error submitting the form. Please try again later.");
-        });
+// Function to reset the form
+function resetForm() {
+    document.getElementById("contactForm").reset();
 }
 
-document.getElementById("contactForm").addEventListener("submit", handleFormSubmission);
+
 
 
